6591eadd0c2deafb00f0693f02c06cc5a7cd34f9
jason <jason@Champ.jason>
2018-08-21 14:37:09 -0400

    Better Extraction of ECE Undergraduate Handbook pdf text

--- ENG_UG_HBs/ECE_Corpus.py
+++ ENG_UG_HBs/ECE_Corpus.py
@@ -1,2 +1,12 @@
 import subprocess
-subprocess.call(['pdftotext', 'ECE_current-handbook.pdf', 'output.txt'])
+#subprocess.call(['pdftohtml', 'ECE_current-handbook.pdf'])
+
+sauce = open('/home/jason/Repo/Virtual-Assistant/ENG_UG_HBs/ECE_current-handbooks.html')
+
+textFile = open('ECE_current-handbook.txt', 'w')
+
+import bs4 as bs
+
+soup = bs.BeautifulSoup(sauce, 'lxml')
+
+textFile.write(soup.body.text)
